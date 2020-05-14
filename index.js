b552bbfeab7d8ce9b357f5c77cca3897f4e74424
jothang <jthang3@gmail.com>
2020-05-13 21:53:36 -0400

    5/13/2019

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ let getCurrent = (e)=>{
     //prevent POST HTTP
     e.preventDefault();
 
+        //adding api ending to find specific information
     const newApi = `${api}?t=${teamName.value}`;
     fetch(newApi)
     .then((value)=>{
@@ -26,8 +27,46 @@ let getCurrent = (e)=>{
     .catch(console.log);
 }
 let doSomething = (jasonData)=>{
-    console.log(jasonData.strTeamLogo);
-    let imgSource = "https://www.thesportsdb.com/"+jasonData.strTeamLogo+".url";
-    console.log(imgSource);
-    result.style.backgroundImage = "url('imgSource')";
+    //removing any child from reult.
+    while(result.firstChild){
+        result.removeChild(result.firstChild);
+    }
+
+
+    //retrieving data from API.
+    let imgSource = jasonData.strTeamLogo;
+    result.style.backgroundImage = `url(${ imgSource })`;
+    let displayTeamName = jasonData.strTeam;
+    let teamCountry = jasonData.strCountry;
+    let teamStatium = jasonData.strStadium;
+    let teamLocation = jasonData.strStadiumLocation;
+    let teamLeague = jasonData.strLeague;
+    let myTeam = [displayTeamName,teamCountry,teamStatium,teamLocation,teamLeague];
+    
+
+    //displaying data from api to the page.
+    let mainDisplay = document.createElement("ul");
+    for(let i in myTeam){
+        let intro;
+        if(i == 0){
+            intro = "Team Name: ";
+        }
+        else if(i == 1){
+            intro = "Team Country: ";
+        }
+        else if(i == 2){
+            intro = "Stadium: ";
+        }
+        else if(i == 3){
+            intro = "Location: ";
+        }
+        else{
+            intro = "Current League: ";
+        }
+        let current = myTeam[i];
+        let newChild = document.createElement("li");
+        newChild.innerHTML = `<h3>${intro}${current}</h3>`;
+        mainDisplay.appendChild(newChild);
+    }
+    result.appendChild(mainDisplay);
 }
